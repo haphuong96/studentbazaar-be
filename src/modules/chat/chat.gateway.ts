dacab51b508d519c1e7d4bc67f75459ae9af6b54
phuong <tnguyen09@qub.ac.uk>
2023-08-02 22:16:35 +0700

    STUD-13 socket jwt

--- src/modules/chat/chat.gateway.ts
+++ src/modules/chat/chat.gateway.ts
@@ -5,32 +5,54 @@ import {
   WebSocketGateway,
   WebSocketServer,
   WsResponse,
+  OnGatewayConnection,
+  WsException,
 } from '@nestjs/websockets';
-import { Injectable, UseGuards } from '@nestjs/common';
+import { Injectable, UseGuards, UseFilters } from '@nestjs/common';
 import { Socket, Server } from 'socket.io';
 import { ChatService } from './chat.service';
 import { WsJwtAuthGuard } from '../auth/guards/ws-auth.guard';
 import { ITokenPayload } from '../auth/auth.interface';
 import { ClientData } from './chat.interface';
+import { ErrorCode } from 'src/common/exceptions/constants.exception';
+import { WsExceptionsFilter } from 'src/common/exceptions/filter.exception';
 
 @Injectable()
-@UseGuards(WsJwtAuthGuard)
+// @UseGuards(WsJwtAuthGuard)
 @WebSocketGateway({ cors: { origin: 'http://localhost:5173' } })
-export class ChatGateway {
+export class ChatGateway implements OnGatewayConnection {
   @WebSocketServer()
   server: Server;
 
   constructor(private chatService: ChatService) {}
+
+  /**
+   * Verify client on connection
+   * @param socket
+   * @param args
+   */
+  @UseFilters(new WsExceptionsFilter())
+  async handleConnection(socket: Socket, ...args: any[]) {
+    console.log('client connected', socket.id);
+
+    const user = await this.chatService.retrieveUserFromSocket(socket);
+
+    if (!user) {
+      socket.disconnect();
+      // throw new WsException(ErrorCode.UNAUTHORIZED);
+    }
+
+    console.log(user)
+    socket.handshake.auth.user = {...user};
+  }
+
   @SubscribeMessage('message')
   onMessage(
     @MessageBody() data: ClientData,
     @ConnectedSocket() socket: Socket,
-  ): void //   WsResponse<void>
-  {
-    // Verify user is authenticated
+  ): void { //   WsResponse<void>
     // If authenticated, send message to all clients & store messages
-    const user: ITokenPayload = data.user;
-
+    const user: ITokenPayload = socket.handshake.auth.user;
     //create conversation if not exists
 
     //save message
--- src/modules/chat/chat.service.ts
+++ src/modules/chat/chat.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { Socket } from 'socket.io';
+import { JWTTokensUtility } from '../auth/utils/jwt-token.util';
+import { ITokenPayload } from '../auth/auth.interface';
+import { WsException } from '@nestjs/websockets';
 
 @Injectable()
 export class ChatService {
-    saveMessage(senderId: number, message: string) {
+  constructor(private jwtService: JWTTokensUtility) {}
+  saveMessage(senderId: number, message: string) {
+    
+  }
 
-    }
+  async retrieveUserFromSocket(socket: Socket) {
+    const [type, token] =
+      socket.handshake.headers.authorization?.split(' ') ?? [];
+
+    // const { token } = socket.handshake.auth;
+    console.log('token', token);
+
+    const user: ITokenPayload = await this.jwtService.verifyAccessToken(token);
+
+    return user;
+  }
 }
